3572baa39829d4f9f7d482e36186f81509bd996f
alex6744 <alexngai6744@gmail.com>
2021-04-18 10:59:13 -0700

    update methods

--- services/questions-service.js
+++ services/questions-service.js
@@ -1,18 +1,22 @@
 const questions = require('./questions.json')
+const questionsModel=require("../db/quesstions/questions-model")
 const createQuestion=()=>{}
 const createQuestionForQuiz=()=>{}
 const findAllQuestions=()=>{
-    return questions
+    //return questions
+    return questionsModel.find()
 }
 const findAllQuestionsForQuiz=(qzid)=>{
-    return questions.filter((question)=>{
-        return question.quizId===qzid;
-    })
+    // return questions.filter((question)=>{
+    //     return question.quizId===qzid;
+    // })
+    return questionsModel.find({quizId:qzid})
 }
 const findQuestionById=(qid)=>{
-    return questions.find((question)=>{
-        return question._id===qid
-    })
+    // return questions.find((question)=>{
+    //     return question._id===qid
+    // })
+    return questionsModel.findById(qid)
 }
 const updateQuestion=()=>{}
 const deleteQuestion=()=>{}
