7e9001bf38571d6f995efe94232c33ca156745bf
alex6744 <alexngai6744@gmail.com>
2021-04-18 10:59:17 -0700

    update methods

--- controllers/questions-controller.js
+++ controllers/questions-controller.js
@@ -2,13 +2,21 @@ const questionService=require("../services/questions-service")
 module.exports=(app)=>{
 
     const findAllQuestions=(req,res)=>{
-        const questions=questionService.findAllQuestions()
-        res.send(questions)
+        // const questions=questionService.findAllQuestions()
+        // res.send(questions)
+        questionService.findAllQuestions()
+            .then((questions)=>{
+                res.send(questions)
+            })
     }
     const findQuestionsForQuiz=(req,res)=>{
         const quizId=req.params.qzid;
-        const questions=questionService.findAllQuestionsForQuiz(quizId);
-        res.send(questions)
+        // const questions=questionService.findAllQuestionsForQuiz(quizId);
+        // res.send(questions)
+        questionService.findAllQuestionsForQuiz(quizId)
+            .then((questions)=>{
+                res.send(questions)
+            })
 
     }
     app.get("/api/questions",findAllQuestions);
