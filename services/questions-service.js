d6239433bc74298bd9d2c9c01113ee050a38b636
alex6744 <alexngai6744@gmail.com>
2021-04-19 07:19:21 -0700

    change method details from dao

--- services/questions-service.js
+++ services/questions-service.js
@@ -1,22 +1,26 @@
-const questions = require('./questions.json')
-const questionsModel=require("../db/models/quesstions/questions-model")
+// const questions = require('./questions.json')
+// const questionsModel=require("../db/models/quesstions/questions-model")
+const questionsDao=require("../db/daos/questions-dao")
 const createQuestion=()=>{}
 const createQuestionForQuiz=()=>{}
 const findAllQuestions=()=>{
     //return questions
-    return questionsModel.find()
+   // return questionsModel.find()
+    return questionsDao.findAllQuestions()
 }
-const findAllQuestionsForQuiz=(qzid)=>{
+const findQuestionsForQuiz=(qzid)=>{
     // return questions.filter((question)=>{
     //     return question.quizId===qzid;
     // })
-    return questionsModel.find({quizId:qzid})
+    //return questionsModel.find({quizId:qzid})
+    return questionsDao.findQuestionsForQuiz(qzid)
 }
 const findQuestionById=(qid)=>{
     // return questions.find((question)=>{
     //     return question._id===qid
     // })
-    return questionsModel.findById(qid)
+    //return questionsModel.findById(qid)
+    return questionsDao.findQuestionById(qid)
 }
 const updateQuestion=()=>{}
 const deleteQuestion=()=>{}
@@ -28,5 +32,5 @@ module.exports = {
     updateQuestion,
     deleteQuestion,
     createQuestionForQuiz,
-    findAllQuestionsForQuiz
+    findQuestionsForQuiz
 }
--- services/quiz-service.js
+++ services/quiz-service.js
@@ -1,18 +1,22 @@
-const quizzes = require('./quizzes.json')
-const quizzesModel=require("../db/models/quizzes/quizzes-model")
+// const quizzes = require('./quizzes.json')
+// const quizzesModel=require("../db/models/quizzes/quizzes-model")
+
+const quizzesDao=require("../db/daos/quizzes-dao")
 
 
 const createQuiz=()=>{}
 const updateQuiz=()=>{}
 const deleteQuiz=()=>{}
 const findAllQuizzes = () => {
-    return quizzesModel.find()
+    //return quizzesModel.find()
+    return quizzesDao.findAllQuizzes()
 }
 const findQuizById = (quizId) =>
     // quizzes.find(quiz => quiz._id === quizId)
-    quizzesModel.findById(quizId)
-        .populate("questions")
-        .exec()
+    // quizzesModel.findById(quizId)
+    //     .populate("questions")
+    //     .exec()
+    quizzesDao.findQuizById(quizId)
 
 module.exports = {
     findAllQuizzes,
