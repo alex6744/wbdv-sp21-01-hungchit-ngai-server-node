66ebbafc797e52c60bd2aeac9b404730e6cb070c
alex6744 <alexngai6744@gmail.com>
2021-04-19 07:18:55 -0700

    add new method

--- db/daos/questions-dao.js
+++ db/daos/questions-dao.js
@@ -1,8 +1,7 @@
 const questionsModel = require('../models/questions/questions-model')
-const quizzesModel = require('../models/quizzes/quizzes-model')
+//const quizzesModel = require('../models/quizzes/quizzes-model')
 
 const findAllQuestions = () => questionsModel.find()
 const findQuestionById = (qid) => questionsModel.findById(qid)
-const findQuestionsForQuiz = (qzid) => quizzesModel.findById(qzid)
-    .populate('questions').then(quiz => quiz.questions)
+const findQuestionsForQuiz = (qzid) =>questionsModel.find({quizId:qzid})
 module.exports = { findAllQuestions, findQuestionById, findQuestionsForQuiz }
